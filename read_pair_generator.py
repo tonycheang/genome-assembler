725b86b4fae8e6a579afe9a76a84d5862a4f662d
Tony Cheang <cheangytony@gmail.com>
2019-05-26 17:47:51 -0700

    added ability to use input args with this program via argparse

--- read_pair_generator.py
+++ read_pair_generator.py
@@ -1,27 +1,52 @@
 #python3
 
 import sys
+import argparse
 from random import seed, randint
 
-""" Read (or Read Pair) Generator
-    Generates a set of random reads given a genome.
+""" Substring (or Substring Pair) Generator
+    Generates a set of random substrings given a circular string.
+    Output:
+        Number of lines on the first line
+        Each subsequent line follows this formatting:
+            Paired - "substring1|substring2|mean_distance"
+            Unpaired - "substring"
 """
 
 
-def read_input():
+def read_args():
+    parser = argparse.ArgumentParser(
+        description="Generates random substrings of fixed length from an circular string, \
+            with the option for paired-substrings drawn uniformly from an average distance away. \
+            Introduces a substitution error with 1% chance.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('length', type=int, help="length of generated substrings")
+    parser.add_argument('num_reads', type=int, help="number of substrings to generate")
+    parser.add_argument('-p', '--paired', action='store_true',
+                        help="allows for paired substrings to be drawn from a distance away", default=False)
+    parser.add_argument('-d', '--distance', type=int, nargs='?',
+                        help="mean distance to draw paired substring from", default=3)
+    parser.add_argument('-e', '--error', type=int, nargs='?',
+                        help="maximum error around mean distance to paired substring, i.e. distance +/- delta", default=0)
+    args = parser.parse_args()
+    print(args)
+    return args
+
+
+def read_string_from_stdin():
     return sys.stdin.readline().strip()
 
 
-def generate_reads(genome, read_len=100, num_reads=34000, paired=False, d=0, delta=3):
+def generate_reads(string, read_len=100, num_reads=34000, paired=False, d=0, delta=3):
     seed()
     BASES = ['A', 'T', 'C', 'G']
     reads = list()
     for _ in range(num_reads):
-        start_pos = randint(0, len(genome))
+        start_pos = randint(0, len(string))
         last_pos = start_pos+read_len
-        read = genome[start_pos:min(last_pos, len(genome))]
-        if last_pos > len(genome):
-            read += genome[0:last_pos % len(genome)]
+        read = string[start_pos:min(last_pos, len(string))]
+        if last_pos > len(string):
+            read += string[0:last_pos % len(string)]
 
         # Introduce a substitution error
         if (randint(0, 100//read_len - 1) == 0):
@@ -29,20 +54,20 @@ def generate_reads(genome, read_len=100, num_reads=34000, paired=False, d=0, del
             read[randint(0, len(read) - 1)] = BASES[randint(0, 3)]
             read = "".join(read)
 
-        assert len(read) == read_len, "READ GENERATED IS SHORT"
+        assert len(read) == read_len, "STRING GENERATED IS SHORT"
 
         if not paired:
             reads.append(read)
         else:
             paired_start = (
-                start_pos + d + randint(-delta, delta)) % len(genome)
+                start_pos + d + randint(-delta, delta)) % len(string)
             paired_last_pos = paired_start + read_len
-            paired_read = genome[paired_start:min(
-                paired_last_pos, len(genome))]
-            if paired_last_pos > len(genome):
-                paired_read += genome[0:paired_last_pos % len(genome)]
+            paired_read = string[paired_start:min(
+                paired_last_pos, len(string))]
+            if paired_last_pos > len(string):
+                paired_read += string[0:paired_last_pos % len(string)]
 
-            assert len(paired_read) == read_len, "READ GENERATED IS SHORT"
+            assert len(paired_read) == read_len, "SUBSTRING GENERATED IS SHORT"
             reads.append("|".join([read, paired_read, str(d)]))
     return reads
 
@@ -54,16 +79,12 @@ def output_reads(reads):
 
 
 def main():
-    genome = read_input()
-    reads = generate_reads(genome, paired=True,
-                           read_len=100, num_reads=700000, d=300, delta=3)
+    args = read_args()
+    string = read_string_from_stdin()
+    reads = generate_reads(string, paired=args.paired,
+                           read_len=args.length, num_reads=args.num_reads, d=args.distance, delta=args.error)
     output_reads(reads)
 
 
 if __name__ == "__main__":
     main()
-
-'''
-Test string (length 30):
-ACTGGGAAACCCTGGCCATGCCAGTACGAG
-'''
